fddf557af9f1798e0082d8a7b57caf30c83c52ec
chinazhoujunbo <760548276@qq.com>
2023-05-18 01:22:25 +0800

    首页

---
+++ src/views/home/components/home-nav-bar.vue
@@ -0,0 +1,27 @@
+<script setup>
+const { title } = defineProps(['title']);
+</script>
+
+<template>
+    <div class="nav-bar">
+        <slot>
+            <div class="title">{{ title }}</div>
+        </slot>
+    </div>
+</template>
+
+<style lang="less" scoped>
+.nav-bar {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height :46px;
+    border-bottom: 1px solid #f3f3f3;
+}
+
+.title {
+    color: var(--primary-color);
+    font-size: 18px;
+    font-weight: 700;
+}
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,13 +1,56 @@
 <script setup>
 
+  import HomeNavBar from '@/views/home/components/home-nav-bar.vue';
+
+  const title = '弘源旅途';
 </script>
 
 <template>
    <div class="home">
-       <h2>Home</h2>
+       <home-nav-bar :title="title"/>
+       <div class="banner">
+           <img src="@/assets/img/home/banner.webp" alt="" />
+       </div>
+       <div class="location">
+           <div class="city">广州</div>
+           <div class="position">
+               <span class="text">我的位置</span>
+               <img src="@/assets/img/home/icon_location.png" alt=""/>
+           </div>
+       </div>
    </div>
 </template>
 
 <style lang="less" scoped>
+
+   .banner {
+     img {
+       width: 100%;
+     }
+   }
+
+   .location {
+     display: flex;
+     justify-content: center;
+     align-items: center;
+     height: 44px;
+     padding: 0 20px;
+
+     .city {
+       flex: 1;
+     }
+
+     .position {
+       display: flex;
+       align-items: center;
+       width: 80px;
+
+       img {
+           margin-left: 5px;
+           width: 18px;
+           height: 18px;
+       }
+     }
+   }
 
 </style>
